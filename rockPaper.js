6c2433ace0dccaca761f3a1140c550315323a3bd
sadrisso <sadriisso304@gmail.com>
2024-05-17 18:12:13 +0600

    add a draw game function

--- rockPaper.js
+++ rockPaper.js
@@ -1,12 +1,31 @@
 
 let choices = document.querySelectorAll('.choice')
+let msg = document.querySelector('h2');
+
 
 choices.forEach((choice) => {
     choice.addEventListener('click', () => {
         let userChoice = choice.getAttribute('id');
         console.log(userChoice);
+        playGame(userChoice)
     });
 });
+
+
+const playGame = (userChoice) => {
+    let compChoice = genCompChoice();
+
+    if (userChoice === compChoice) {
+        drawGame();
+    }
+}
+
+
+const drawGame = () => {
+    msg.innerText = 'Draw';
+    msg.style.backgroundColor = 'yellow';
+    msg.style.color = 'black';
+}
 
 
 const genCompChoice = () => {
