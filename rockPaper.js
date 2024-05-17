8d266eaf15d653861b8da99040ef18c118038bb6
sadrisso <sadriisso304@gmail.com>
2024-05-17 14:39:39 +0600

    generate computer choice

--- rockPaper.js
+++ rockPaper.js
@@ -4,6 +4,13 @@ let choices = document.querySelectorAll('.choice')
 choices.forEach((choice) => {
     choice.addEventListener('click', () => {
         let userChoice = choice.getAttribute('id');
-        console.log(userChoice)
-    })
-})
+        console.log(userChoice);
+    });
+});
+
+
+const genCompChoice = () => {
+    let idx = Math.floor(Math.random() * 3);
+    let options = ['rock', 'paper', 'scissor'];
+    return options[idx];
+}
