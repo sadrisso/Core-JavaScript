ea12b5d3beeab4cc47075698f048a9476b3f8222
sadrisso <sadriisso304@gmail.com>
2024-05-17 19:16:40 +0600

    showing the winner

--- rockPaper.js
+++ rockPaper.js
@@ -3,33 +3,58 @@ let choices = document.querySelectorAll('.choice')
 let msg = document.querySelector('h2');
 
 
-choices.forEach((choice) => {
+choices.forEach((choice) => 
+{
     choice.addEventListener('click', () => {
         let userChoice = choice.getAttribute('id');
-        console.log(userChoice);
         playGame(userChoice)
     });
 });
 
+const drawGame = () => 
+{
+    msg.innerText = 'Draw';
+    msg.style.color = 'yellow';
+}
 
-const playGame = (userChoice) => {
+
+const playGame = (userChoice) => 
+{
     let compChoice = genCompChoice();
+    console.log(userChoice)
+    console.log(compChoice)
 
     if (userChoice === compChoice) {
         drawGame();
-    } 
+    } else {
+        let userWin = true;
+        if(userChoice === 'rock') {
+            userWin = compChoice === 'scissor' ? true : false;
+        } else if (userChoice === 'paper') {
+            userWin = compChoice === 'rock' ? true : false;
+        } else {
+            userWin = compChoice === 'paper' ? true : false;
+        }
+        showWinner(userWin, compChoice, userChoice);
+    }
 }
 
 
-const drawGame = () => {
-    msg.innerText = 'Draw';
-    msg.style.backgroundColor = 'yellow';
-    msg.style.color = 'black';
-}
-
-
-const genCompChoice = () => {
+const genCompChoice = () => 
+{
     let idx = Math.floor(Math.random() * 3);
     let options = ['rock', 'paper', 'scissor'];
     return options[idx];
+}
+
+
+const showWinner = (userWin, compChoice, userChoice) => 
+{
+    if (userWin) {
+        msg.innerText = `Congratulations, your ${userChoice} beats computer ${compChoice}`;
+        msg.style.color = 'Green';
+    } else {
+        msg.innerText = `You lost, computer ${compChoice} beats your ${userChoice}`;
+        msg.style.color = 'red';
+    }
 }
