88f44ce2a2436732e7c365f969fa2b872fe346e3
sadrisso <sadriisso304@gmail.com>
2024-05-18 11:57:57 +0600

    add attempts

--- guessingGame.js
+++ guessingGame.js
@@ -3,12 +3,25 @@ let form = document.querySelector('form');
 let userChoice = document.querySelector('.userChoice');
 let msg = document.querySelector('.msg-container');
 let container = document.querySelector('.container')
+let attempts = document.querySelector('.attempts')
+let attempt = 0;
+let totalAttempts = 5;
 
 form.addEventListener('submit', (evt) => 
 {
     evt.preventDefault();
-    let userGuess = Number(userChoice.value)
-    playGame(userGuess)
+    attempt++;
+    if (attempt > 4) 
+    {
+        userChoice.disabled = true;
+    }
+    if (attempt < 6) 
+    {
+        let userGuess = Number(userChoice.value)
+        playGame(userGuess)
+    }
+
+    userChoice.value = '';
 })
 
 
@@ -24,13 +37,19 @@ const playGame = (userGuess) =>
     // console.log(`User choice is ${userGuess}`)
     // console.log(`comp choice is ${randomNumber}`)
 
-    if (userGuess === randomNumber) {
+    if (userGuess === randomNumber) 
+    {
         console.log(`You win, your choice ${userGuess} & Computer choice ${randomNumber}`)
         msg.innerText = `You win, your choice ${userGuess} & Computer choice ${randomNumber}`
         container.style.backgroundColor = 'green';
-    } else {
+        container.innerText = `Remaining attempts = ${totalAttempts} - ${attempt}`
+    } 
+    else 
+    {
         console.log(`You lost, your choice ${userGuess} & Computer choice ${randomNumber}`)
         msg.innerText = `You lost, your choice ${userGuess} & Computer choice ${randomNumber}`
         container.style.backgroundColor = 'red';
+        container.innerText = `Remaining attempts = ${totalAttempts} - ${attempt}`
     }
+    
 } 
