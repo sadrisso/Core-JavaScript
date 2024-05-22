59fb6c5b20d698f4038bd3672e24c11a7c45a119
sadrisso <sadriisso304@gmail.com>
2024-05-22 14:33:30 +0600

    try to modify guessing game with jQuery

--- guessingGame.js
+++ guessingGame.js
@@ -1,54 +1,66 @@
 
-let form = document.querySelector('form');
-let userChoice = document.querySelector('.userChoice');
-let msg = document.querySelector('.msg-container');
-let container = document.querySelector('.container')
-let attempts = document.querySelector('.attempts')
-let attempt = 0;
-let totalAttempts = 5;
+// let form = document.querySelector('form');
+// let userChoice = document.querySelector('.userChoice');
+// let msg = document.querySelector('.msg-container');
+// let container = document.querySelector('.container')
+// let attempts = document.querySelector('.attempts')
+// let attempt = 0;
+// let totalAttempts = 5;
 
 
-form.addEventListener('submit', (evt) => 
+// form.addEventListener('submit', (evt) => 
+// {
+//     evt.preventDefault();
+//     attempt++;
+//     if (attempt > 4) 
+//     {
+//         userChoice.disabled = true;
+//     }
+//     if (attempt < 6) 
+//     {
+//         let userGuess = Number(userChoice.value)
+//         playGame(userGuess)
+//     }
+
+//     userChoice.value = '';
+// })
+
+
+// const genRandomNumber = () => 
+// {
+//     return Math.floor(Math.random() * 5 + 1)
+// }
+
+// const playGame = (userGuess) => 
+// {
+//     let randomNumber = genRandomNumber();
+
+//     // console.log(`User choice is ${userGuess}`)
+//     // console.log(`comp choice is ${randomNumber}`)
+
+//     if (userGuess === randomNumber) 
+//     {
+//         console.log(`You win, your choice ${userGuess} & Computer choice ${randomNumber}`)
+//         msg.innerText = `You win, your choice ${userGuess} & Computer choice ${randomNumber}`
+//         container.style.backgroundColor = 'green';
+//     } 
+//     else 
+//     {
+//         console.log(`You lost, your choice ${userGuess} & Computer choice ${randomNumber}`)
+//         msg.innerText = `You lost, your choice ${userGuess} & Computer choice ${randomNumber}`
+//         container.style.backgroundColor = 'red';
+//     }
+    
+// } 
+
+
+let form = $('form');
+let userChoice = $('.userChoice')
+
+
+form.submit((evt) => 
 {
     evt.preventDefault();
-    attempt++;
-    if (attempt > 4) 
-    {
-        userChoice.disabled = true;
-    }
-    if (attempt < 6) 
-    {
-        let userGuess = Number(userChoice.value)
-        playGame(userGuess)
-    }
-
-    userChoice.value = '';
+    let userValue = Number(userChoice.val());
+    console.log(userValue);
 })
-
-
-const genRandomNumber = () => 
-{
-    return Math.floor(Math.random() * 5 + 1)
-}
-
-const playGame = (userGuess) => 
-{
-    let randomNumber = genRandomNumber();
-
-    // console.log(`User choice is ${userGuess}`)
-    // console.log(`comp choice is ${randomNumber}`)
-
-    if (userGuess === randomNumber) 
-    {
-        console.log(`You win, your choice ${userGuess} & Computer choice ${randomNumber}`)
-        msg.innerText = `You win, your choice ${userGuess} & Computer choice ${randomNumber}`
-        container.style.backgroundColor = 'green';
-    } 
-    else 
-    {
-        console.log(`You lost, your choice ${userGuess} & Computer choice ${randomNumber}`)
-        msg.innerText = `You lost, your choice ${userGuess} & Computer choice ${randomNumber}`
-        container.style.backgroundColor = 'red';
-    }
-    
-} 
