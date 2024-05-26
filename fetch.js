376abb3ea2d39ee70347f88535895ca2b9f2ab1c
sadrisso <sadriisso304@gmail.com>
2024-05-26 21:44:55 +0600

    fetch data and display data

--- fetch.js
+++ fetch.js
@@ -1,9 +1,17 @@
 
 
 let URL = 'https://catfact.ninja/fact';
+let body = document.querySelector('.body');
+let btn = document.querySelector('.btn');
 
 
-const getFacts = async () => {
-    let promise = await fetch(URL);
-    console.log(promise)
-}
+const getFacts = async () => 
+{
+    console.log('Getting data...')
+    let response = await fetch(URL);
+    console.log(response)
+    let data = await response.json();
+    body.innerText = data.fact;
+};
+
+btn.addEventListener('click', getFacts);
