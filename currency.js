13dce38a655371bc58c131ef1d79db0a006f1f5d
sadrisso <sadriisso304@gmail.com>
2024-05-29 23:03:29 +0600

    add multiple options and add auto selection

--- currency.js
+++ currency.js
@@ -4,34 +4,68 @@ let BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/cu
 let dropdowns = document.querySelectorAll('.dropdown select');
 
 
+
 for (select of dropdowns) 
 {
-    for (currCode in countryList) 
+    for (currCode in countryList)
     {
-        let newOption = document.createElement('option');
+        let newOption = document.createElement("option")
         newOption.innerText = currCode;
         newOption.value = currCode;
-
-        if (select.name === "from" && currCode === "USD") {
-            newOption.selected = "selected";
-          } else if (select.name === "to" && currCode === "BDT") {
-            newOption.selected = "selected";
-          }
-
         select.append(newOption);
     }
-
-    select.addEventListener('change', (evt) => 
-    {
-        update_flag(evt.target)
-    });
 }
 
-const update_flag = (element) => 
-{
-    let currCode = element.value;
-    let countryCode = countryList[currCode];
-    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-    let img = element.parentElement.querySelector('img');
-    img.src = newSrc;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (select of dropdowns) 
+// {
+//     for (currCode in countryList) 
+//     {
+//         let newOption = document.createElement('option');
+//         newOption.innerText = currCode;
+//         newOption.value = currCode;
+
+//         if (select.name === "from" && currCode === "USD") {
+//             newOption.selected = "selected";
+//           } else if (select.name === "to" && currCode === "BDT") {
+//             newOption.selected = "selected";
+//           }
+
+//         select.append(newOption);
+//     }
+
+//     select.addEventListener('change', (evt) => 
+//     {
+//         update_flag(evt.target)
+//     });
+// }
+
+// const update_flag = (element) => 
+// {
+//     let currCode = element.value;
+//     let countryCode = countryList[currCode];
+//     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+//     let img = element.parentElement.querySelector('img');
+//     img.src = newSrc;
+// }
