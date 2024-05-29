052ebe9da5fe9433b7445af138a0febaef36e926
sadrisso <sadriisso304@gmail.com>
2024-05-29 22:43:19 +0600

    add update flag option and auto select option

--- currency.js
+++ currency.js
@@ -4,12 +4,34 @@ let BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/cu
 let dropdowns = document.querySelectorAll('.dropdown select');
 
 
-for (select of dropdowns) {
-    for (currCode in countryList) {
+for (select of dropdowns) 
+{
+    for (currCode in countryList) 
+    {
         let newOption = document.createElement('option');
         newOption.innerText = currCode;
         newOption.value = currCode;
-        console.log(newOption)
+
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected";
+          } else if (select.name === "to" && currCode === "BDT") {
+            newOption.selected = "selected";
+          }
+
         select.append(newOption);
     }
+
+    select.addEventListener('change', (evt) => 
+    {
+        update_flag(evt.target)
+    });
+}
+
+const update_flag = (element) => 
+{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector('img');
+    img.src = newSrc;
 }
