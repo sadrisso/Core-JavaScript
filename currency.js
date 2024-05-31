e79fa00301d91cff342164aeda2bc01c39183bdf
sadrisso <sadriisso304@gmail.com>
2024-05-31 15:19:48 +0600

    make the select tag options dynamic

--- currency.js
+++ currency.js
@@ -1,18 +1,16 @@
 
 let BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
 let dropdowns = document.querySelectorAll('.dropdown select');
 
 
-
-for (select of dropdowns) 
+ for (select of dropdowns)
 {
     for (currCode in countryList)
     {
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value = currCode;
-        select.append(newOption)
+        select.append(newOption);
 
         if (select.name === "from" && currCode === "USD") {
             newOption.selected = "selected";
@@ -20,34 +18,6 @@ for (select of dropdowns)
             newOption.selected = "selected";
         }
     }
-
-    select.addEventListener("change", (evt) => {
-        update_flag(evt.target)
-    })
-}
-
-
-const update_flag = (element) => 
-{
-    let currCode = element.value;
-    let countryCode = countryList[currCode];
-    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-    let img = element.parentElement.querySelector("img");
-    img.src = newSrc;
-}
-
-
-
-let obj = {
-    name: 'drisso',
-    age: 23,
-    skill: 'nothing'
-}
-
-console.log(obj)
-
-for (o in obj) {
-    console.log(obj[o])
 }
 
 
@@ -58,22 +28,28 @@ for (o in obj) {
 
 
 
+// clearing the object concept with for in loop...
+
+// let obj = {
+//     name: 'drisso',
+//     age: 23,
+//     skill: 'nothing'
+// }
+
+// console.log(obj)
+
+// for (o in obj) {
+//     console.log(obj[o])
+// }
 
 
+// let arr = ['apple', 'banana', 'watermallon', 'goava'];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// for (let a in arr) {
+//     // console.log(arr)
+//     // console.log(arr[a])
+//     console.log(a)
+// }
 
 
 
@@ -96,27 +72,6 @@ for (o in obj) {
 //         select.append(newOption);
 //     }
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
