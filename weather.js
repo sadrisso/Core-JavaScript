00ae11061746a0bacc92bd95f7b562da2d3788ec
sadrisso <sadriisso304@gmail.com>
2024-06-02 11:47:26 +0600

    select all element and display with the api data

--- weather.js
+++ weather.js
@@ -1,11 +1,20 @@
 
 const apiKey = "appid=e1802a4312942c2f7c8c4edb617eb85a";
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=dhaka&${apiKey}`;
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
 
 
-const get_weather_data = async () => 
+const get_weather_data = (async () => 
 {
     let response = await fetch(apiUrl);
     let data = await response.json();
     console.log(data)
-}
+
+    city.innerHTML = data.name;
+    temp.innerHTML = data.main.temp;
+    humidity.innerHTML = data.main.humidity;
+    windSpeed.innerHTML = data.wind.speed;
+})();
