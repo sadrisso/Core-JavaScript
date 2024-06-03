798aa183a8aa291735f76ce69ac7f902ecb6fb26
sadrisso <sadriisso304@gmail.com>
2024-06-03 12:30:38 +0600

    fetching & displaing data

--- weatherApp.js
+++ weatherApp.js
@@ -1,15 +1,28 @@
 
 const apiKey = "e1802a4312942c2f7c8c4edb617eb85a";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const temp = document.querySelector(".temp");
+const city = document.querySelector(".city");
+const humidity = document.querySelector(".humidity")
+let windSpeed = document.querySelector(".wind");
 
 
-const get_weather_data = async (city) => 
+const get_weather_data = (async (city) => 
 {
     let response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    console.log(response)
     let data = await response.json();
     console.log(data)
+
+    display_weather_data(data)
+
+})("rangpur")
+
+
+
+const display_weather_data = (data) => 
+{
+    temp.innerHTML = data.main.temp;
+    humidity.innerHTML = data.main.humidity + "%";
+    city.innerHTML = data.name;
+    windSpeed.innerHTML = data.wind.speed + " km/h";
 }
-
-
-get_weather_data("rangpur")
