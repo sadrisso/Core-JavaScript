1253167fc422073a7a8ecad2b43db20a82a6e8b3
sadrisso <sadriisso304@gmail.com>
2024-06-05 12:15:39 +0600

    basic oop object

---
+++ oop.js
@@ -0,0 +1,14 @@
+
+function PartyAnimal () 
+{
+    this.x = 0;
+    this.party = function () {
+        this.x = this.x + 1;
+        console.log("So far " + this.x);
+    }
+}
+
+an = new PartyAnimal();
+an.party()
+an.party()
+an.party()
