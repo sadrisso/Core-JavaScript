c1f1b26955a1c34f0a1126e7da68006fa84b6938
sadrisso <sadriisso304@gmail.com>
2024-06-05 12:28:13 +0600

    basic constructor

--- oop.js
+++ oop.js
@@ -12,3 +12,21 @@ an = new PartyAnimal();
 an.party()
 an.party()
 an.party()
+
+
+function NewAnimal (name, desc)
+{
+    this.x = 0;
+    // this.name = name;
+    // this.desc = desc;
+    this.party = function ()
+    {
+        this.x += 1;
+        console.log(name + " " + desc + " " + this.x);
+    }
+}
+
+let obj = new NewAnimal("Elephant", "is one of the most strongest animal");
+obj.party();
+obj.party();
+obj.party();
