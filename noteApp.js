99544e6fe63797f62594a7964421d1000116d9f9
sadrisso <sadriisso304@gmail.com>
2024-06-11 16:16:17 +0600

    add data to localStorage and display data from localStorage

--- noteApp.js
+++ noteApp.js
@@ -1,11 +1,25 @@
 
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
-const notes = document.querySelectorAll(".input-box");
+let notes = document.querySelectorAll(".input-box");
 const img = document.querySelector(".notes-container img")
 
 
-createBtn.addEventListener("click", () => {
+const show_notes = () =>
+{
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+show_notes();
+
+const update_storage = () =>
+{
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+
+createBtn.addEventListener("click", () =>
+{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
@@ -15,8 +29,18 @@ createBtn.addEventListener("click", () => {
     notesContainer.appendChild(inputBox).appendChild(img);
 });
 
-notesContainer.addEventListener("click", (e) => {
+
+notesContainer.addEventListener("click", (e) =>
+{
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
-    }
-})
+        update_storage();
+    } else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach((n) => {
+            n.onkeyup = function() {
+                update_storage();
+            }
+        });
+    };
+});
