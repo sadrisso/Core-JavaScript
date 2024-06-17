685dd7667534f41164f9d9ee36bfbf3b443c5e23
sadrisso <sadriisso304@gmail.com>
2024-06-17 09:47:31 +0600

    auto text effect animation mini project

---
+++ autoTextEffect.js
@@ -0,0 +1,27 @@
+
+const container = document.querySelector(".container");
+const careers = ["YouTuber", "Web Developer", "Photographer", "Instructor"]
+let careersIdx = 0;
+let charIdx = 0;
+
+
+
+function update_text ()
+{
+    charIdx++;
+
+    container.innerHTML = `<h1>I am a ${careers[careersIdx].slice(0, charIdx)}</h1>`;
+
+    setTimeout(update_text, 200)
+
+    if (charIdx === careers[careersIdx].length) {
+        careersIdx++;
+        charIdx = 0;
+    }
+
+    if (careersIdx === careers.length) {
+        careersIdx == 0;
+    }
+}
+
+update_text();
