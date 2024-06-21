b4d2e4fbb222b9ee77afee14ee1411121d174858
sadrisso <sadriisso304@gmail.com>
2024-06-21 15:26:34 +0600

    setItem to the localStorage and getItem from the localStorage

--- darkModeToggle.js
+++ darkModeToggle.js
@@ -1,16 +1,53 @@
+// const inputEl = document.querySelector(".input");
+// const bodyEl = document.querySelector("body");
+
+// inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+// update_body();
+
+// const update_body = () =>
+// {
+//     if (inputEl.checked) {
+//         bodyEl.style.backgroundColor = "black";
+//     } else {
+//         bodyEl.style.backgroundColor = "white";
+//     }
+// }
+
+
+// inputEl.addEventListener("input", () =>
+// {
+//     update_body();
+//     update_local_storage();
+// });
+
+
+// const update_local_storage = () =>
+//     {
+//         localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+//     }
+
+
 const inputEl = document.querySelector(".input");
+
 const bodyEl = document.querySelector("body");
 
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
-const update_body = () =>
-{
-    if (inputEl.checked) {
-        bodyEl.style.backgroundColor = "black";
-    } else {
-        bodyEl.style.backgroundColor = "white";
-    }
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "black";
+  } else {
+    bodyEl.style.background = "white";
+  }
 }
 
-inputEl.addEventListener("click", () => {
-    update_body();
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
 });
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
